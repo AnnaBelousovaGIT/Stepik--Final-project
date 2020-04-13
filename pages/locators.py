b11627affe4afeca9341dda08adb950a4510d0bd
Belousova <konstantinovnaaa@gmail.com>
2020-04-13 14:49:27 +0400

    Добавлен класс ProductPageLocators(), в нем описаны необходимые локаторы к элементам страницы

--- pages/locators.py
+++ pages/locators.py
@@ -2,9 +2,20 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 
 class LoginPageLocators():
      LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
      REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators():
+     BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+
+     PRODUCT_NAME =(By.TAG_NAME, "h1")
+     BASKET_NAME = (By.CLASS_NAME, "alert.alert-safe.alert-noicon.alert-success.fade.in .alertinner  strong")
+
+     PRODUCT_PRICE = (By.CSS_SELECTOR, "p.price_color")
+     BASKET_PRICE = (By.CLASS_NAME, "alert.alert-safe.alert-noicon.alert-info.fade.in .alertinner  strong")
+
