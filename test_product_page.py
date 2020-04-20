cb34f16d936080d6ce4b00e7e4c5cb0b3c7d783d
Belousova <konstantinovnaaa@gmail.com>
2020-04-20 13:58:06 +0400

    final preparation for review

--- test_product_page.py
+++ test_product_page.py
@@ -4,7 +4,7 @@ from pages.login_page import LoginPage
 import pytest
 import time
 
-
+@pytest.mark.need_review
 @pytest.mark.parametrize('promo_offer',
                          ["0", "1", "3", "4", "5", "6", pytest.param(7, marks=pytest.mark.xfail), "8", "9"])
 def test_guest_can_add_product_to_basket(browser, promo_offer):
@@ -23,7 +23,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
-
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -55,7 +55,7 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.should_be_button_add_to_basket()
     page.should_dissapear_of_success_message()
 
-
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page = BasketPage(browser, link)
     page.open()
@@ -64,7 +64,7 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page.should_not_product_in_basket()
 
 
-@pytest.mark.login_user
+
 class TestUserAddToBasketFromProductPage():
     @pytest.fixture(scope="function", autouse=True)
     def setup(self, browser):
@@ -79,6 +79,7 @@ class TestUserAddToBasketFromProductPage():
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         page = ProductPage(browser, self.link)
         page.open()
