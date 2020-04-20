d961c5f3434ff48362b0563953afce96c3624a7a
Belousova <konstantinovnaaa@gmail.com>
2020-04-20 13:00:41 +0400

    Добавлен метод register_new_user с регистрацией нового пользователя

--- pages/login_page.py
+++ pages/login_page.py
@@ -2,15 +2,22 @@ from .base_page import BasePage
 from .locators import LoginPageLocators
 
 
-
 class LoginPage(BasePage):
+
+    def register_new_user(self, email, password):
+        self.go_to_login_page()
+        self.browser.find_element(*LoginPageLocators.REGISTRATION_EMAIL).send_keys(email)
+        self.browser.find_element(*LoginPageLocators.REGISTRATION_PASSWORD1).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTRATION_PASSWORD2).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.BUTTON_REGISTRATION).click()
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # проверка на корректный url адрес
+        # проверяем, что текущий url адрес содержит подстроку "login"
         assert 'login' in self.browser.current_url, "Login link is not found"
 
     def should_be_login_form(self):
