317d5bcd67667ddde5da2609de048970e9cdf3a7
Belousova <konstantinovnaaa@gmail.com>
2020-04-20 12:58:08 +0400

    Добавлены селекторы для регистрации нового пользователя

--- pages/locators.py
+++ pages/locators.py
@@ -2,28 +2,33 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators():
-     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 
 class LoginPageLocators():
-     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
-     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    REGISTRATION_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTRATION_PASSWORD1 = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTRATION_PASSWORD2 = (By.CSS_SELECTOR, "#id_registration-password2")
+    BUTTON_REGISTRATION = (By.CSS_SELECTOR, "[value='Register']")
 
 
 class ProductPageLocators():
-     BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
-     PRODUCT_NAME =(By.TAG_NAME, ".product_main h1")
-     BASKET_NAME = (By.CLASS_NAME, "alert.alert-safe.alert-noicon.alert-success.fade.in .alertinner  strong")
-     PRODUCT_PRICE = (By.CSS_SELECTOR, "p.price_color")
-     BASKET_PRICE = (By.CLASS_NAME, "alert.alert-safe.alert-noicon.alert-info.fade.in .alertinner  strong")
-     SUCCESS_MESSAGE=(By.CSS_SELECTOR, "div.alertinner ")
+    BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_NAME = (By.TAG_NAME, ".product_main h1")
+    BASKET_NAME = (By.CLASS_NAME, "alert.alert-safe.alert-noicon.alert-success.fade.in .alertinner  strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "p.price_color")
+    BASKET_PRICE = (By.CLASS_NAME, "alert.alert-safe.alert-noicon.alert-info.fade.in .alertinner  strong")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "div.alertinner ")
+
 
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
+
 
 class BasketPageLocators():
-    BUTTON_BASKET_FROM_HEADER_PAGE =(By.CSS_SELECTOR, ".basket-mini > span > a") #кнопка просмотр корзины
-    MESSAGE_OF_EMPTY_BASKET = (By.CSS_SELECTOR, "#content_inner") # сообщение "ваша корзина пуста"
-
-
+    BUTTON_BASKET_FROM_HEADER_PAGE = (By.CSS_SELECTOR, ".basket-mini > span > a")  # кнопка просмотр корзины
+    MESSAGE_OF_EMPTY_BASKET = (By.CSS_SELECTOR, "#content_inner")  # сообщение "ваша корзина пуста"
